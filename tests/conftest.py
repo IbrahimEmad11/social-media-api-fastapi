fd42d39843549f80d8babc0f4285eb0035d6513a
Ibrahim Emad <ibrahimemad.2002x@gmail.com>
2024-07-23 21:11:09 +0300

    Add posts tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 from app.database import get_db ,Base
+from app.oauth2 import create_jwt_token
+from app import models
 
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
@@ -68,3 +70,64 @@ def test_user(client):
     return new_user
 
 
+@pytest.fixture
+def test_user2(client):
+    user_data = {"email": "hima234@gmail.com",
+                 "password": "test123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+
+@pytest.fixture
+def token(test_user):
+    return create_jwt_token({"user_id": test_user['id']})
+
+
+@pytest.fixture
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+
+    return client
+
+
+@pytest.fixture
+def test_posts(test_user, session, test_user2):
+    posts_data = [{
+        "title": "el awl",
+        "content": "awl content",
+        "user_id": test_user['id']
+    }, {
+        "title": "maloosh tany",
+        "content": "tany content",
+        "user_id": test_user['id']
+    },
+        {
+        "title": "el trees khasees",
+        "content": "talet",
+        "user_id": test_user['id']
+    }, {
+        "title": "el trees khasees",
+        "content": "talet",
+        "user_id": test_user2['id']
+    }]
+
+    def create_post_model(post):
+        return models.Posts(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    session.commit()
+
+    posts = session.query(models.Posts).all()
+    return posts
