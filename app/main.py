1d13a2b33d8222910bb854b2c45b8726d84b0eef
Ibrahim Emad <ibrahimemad.2002x@gmail.com>
2024-08-28 14:33:50 +0300

    remove duplicate code

--- app/main.py
+++ app/main.py
@@ -23,26 +23,6 @@ async def main():
 
 # models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
-        
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-@app.get('/')
-def root():
-    return {"message": "Hello World!!"}
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
